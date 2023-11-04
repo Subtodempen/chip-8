2581a8adb51bdff611fa07e1905b12432364d7bd
subtodempen <pehlivandemirel@yahoo.com>
2023-11-04 00:09:21 +0000

    added more functions

--- chip8.hpp
+++ chip8.hpp
@@ -1,7 +1,13 @@
 #include<iostream>
 
 class CPU{
-	
+	public:
+		Cpu();
+		~Cpu();
+
+		void Init();	//sets everyything to zero
+		void loadRom(); //loads ROM instructions into 0x200 onwards
+		void emuCycle();//emulates a cycle
 	private:
 		struct Memory{
 			unsigned char ram[4095];
